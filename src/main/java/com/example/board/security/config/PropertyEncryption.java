d1f2c34e5d85c256d8fb8e89edb50b1347c286ec
giraffeb <rwpakr07@gmail.com>
2018-12-19 20:48:32 +0900

    property encrypt + jenkins

---
+++ src/main/java/com/example/board/security/config/PropertyEncryption.java
@@ -0,0 +1,29 @@
+package com.example.board.security.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertyEncryption {
+
+    //적용해보기.
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword("giraffeb"); //암호화에 사용할 키 -> 중요
+        config.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
